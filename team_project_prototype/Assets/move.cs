975a925d1c513cac3ad03aaecf69c175b0ab2203
takuto-litalico <60427297+takuto-litalico@users.noreply.github.com>
2020-02-07 21:08:56 +0900

    nantoka

---
+++ team_project_prototype/Assets/move.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    public GameObject player;
+    public Vector3 velocity;
+    private CharacterController controller;
+    private Vector3 moveDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 走り
+        if (Input.GetKey(KeyCode.D))
+        {
+            player.transform.position += transform.right * velocity.y * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            player.transform.position -= transform.right * velocity.y * Time.deltaTime;
+        }
+        //ジャンプ
+        if (controller.isGrounded)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                moveDirection.y = 20;
+            }
+        }
+
+        moveDirection.y -= 10 * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+}
---
+++ team_project_prototype/Assets/weapon.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class weapon : MonoBehaviour
+{
+    public GameObject ammo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        GameObject ammos;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            ammos = GameObject.Instantiate(ammo);
+            ammos.transform.position = transform.position;
+        }
+
+        
+    }
+}
