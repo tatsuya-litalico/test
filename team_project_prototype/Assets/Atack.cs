0dd41ea91a8db05e62184b4498d514287036ea0b
Ruchi <rappinngu2006@gmail.com>
2020-02-15 20:23:19 +0900

    no message

---
+++ team_project_prototype/Assets/Atack.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Atack : MonoBehaviour
+{
+    [SerializeField]
+    int density = 0;
+    [SerializeField]
+    GameObject bullet = null;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject b = Instantiate(bullet, transform.position, new Quaternion(0, 0, 0, 0));
+            b.transform.Rotate(0, 0, 180);
+            b = null;
+            //BossAtack();
+        }
+    }
+
+    void BossAtack()
+    {
+        if(density < 1)
+        {
+            return;//0よりしたはエラー起こるので弾く
+        }
+        if(180 % density != 0)
+        {
+            return;//180の約数でなければreturn
+        }
+        for (int rad = 0; rad <= 180; rad += density)
+        {
+            GameObject b = Instantiate(bullet, transform.position, new Quaternion(0, 0, 0, 0));
+            b.transform.Rotate(0, 0, rad);
+            Debug.Log(rad);
+        }
+    }
+}
---
+++ team_project_prototype/Assets/Bullet.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    float speed = 1f;
+    Rigidbody2D rd = default;
+    void Start()
+    {
+        rd = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        rd.AddForce(transform.right * (transform.rotation.z >= 0 ? speed : -speed), ForceMode2D.Impulse);
+    }
+}
